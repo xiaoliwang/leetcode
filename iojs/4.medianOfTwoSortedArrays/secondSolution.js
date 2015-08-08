bf4dab1baafbe4233fb5757a26ec79d631987efb
Tom <jiepengthegreat@126.com>
2015-08-08 17:08:05 +0800

    temp

--- iojs/4.medianOfTwoSortedArrays/secondSolution.js
+++ iojs/4.medianOfTwoSortedArrays/secondSolution.js
@@ -4,28 +4,108 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-	var long_len = nums1.length;
-	var short_len = nums2.length;
+	var short_len = nums1.length;
+	var long_len = nums2.length;
 	
-	if(long_len < short_len){
+	if (long_len < short_len){
 		var temp = nums1;
-		var nums1 = nums2;
-		var nums2 = temp;
+		nums1 = nums2;
+		nums2 = temp;
+		
+		long_len ^= short_len;
+		short_len ^= long_len;
+		long_len ^= short_len;
 	}
 	
-	var half_len  = ((len1 ^ (len2 + 1)) >> 1) + (len1 & (len2 + 1));
+	var len = short_len + long_len;
+	var odd = len & 1;
+	var half_len  = (len + 1) >> 1;
+	
+	nums1[short_len] = Infinity;
+	nums2[long_len] = Infinity;
+	nums1[-1] = -Infinity;
+	nums2[-1] = -Infinity
+	
+	if (short_len === 0)
+		if (odd)
+			return nums2[half_len - 1];
+		else
+			return (nums2[half_len] + nums2[half_len - 1]) / 2;
 	
 	var imax = long_len;
 	var imin = 0;
 	
-	while (imin <= imax) {
-		var i = ((imin ^ imax) >> 1) + (imin & imax);
-		var j = half_len - i;
+	while (imin < imax){
+		var j = (imax + imin + 1) >> 1;
+		if ((i = half_len - j) < 1){
+			i = 1;
+			j = half_len - 1;
+		} else if (i > short_len) {
+			i = short_len;
+			j = half_len - short_len;
+		}
 		
-		if (j > 0 && i < m && nums2[j - 1] > nums1[i])
-			imin = i + 1;
-		else (i > 0 && j < n && nums)
+		if (nums1[i - 1] < nums2[j - 1]){
+			if (nums1[i] >= nums2[j - 1] || i === short_len) {
+				if (odd){
+					return nums2[j - 1];
+				} else {
+					if (nums2[j] && nums1[i] > nums2[j]) {
+						return (nums2[j - 1] + nums2[j]) / 2;
+					} else {
+						return (nums2[j - 1] + nums1[i]) / 2;
+					}
+				}
+			} else if(nums2[j - 2] < 0){
+				if(numsi[i] < nums1[i - 1]){
+					return (nums2[j - 1] + nums1[i - 1]) / 2;
+				} else {
+					return (nums2[j - 1] + nums1[i - 1]) / 2;
+				}
+			} else {
+				imax = (imax > j) ? j : (imax - 1);
+			}
+		} else if (nums1[i - 1] > nums2[j - 1]){
+			if (nums2[j] >= nums1[i - 1]) {
+				if (odd) {
+					return  nums1[i - 1];
+				} else {
+					if (nums2[j] > nums1[i]){
+						return (nums1[i - 1] + nums1[i]) / 2;
+					} else {
+						return (nums1[i - 1] + nums2[j]) / 2;
+					}
+				}
+			} else if (i === 1){
+				if (odd) {
+					return nums2[j];
+				} else {
+					if (nums2[j + 1] < nums1[0]){
+						return (nums2[j + 1] + nums2[j]) / 2;
+					} else {
+						return (nums1[0] + nums2[j]) / 2
+					}
+				}
+			} else {
+				imin = (imin < j) ? j:(imin + 1);
+			}
+		} else {
+			if(odd){
+				return nums1[i - 1];
+			}else{
+				if (nums1[i] > nums2[j]) {
+					return (nums2[j] + nums2[j - 1]) /2;
+				} else {
+					return (nums1[i] + nums2[j - 1]) /2;
+				}
+			}
+			
+		}
 	}
+	return undefined;
 };
 
 module.exports = findMedianSortedArrays;
+
+var args = {ones: [1,2], twos: [3,4], expected: 2.5};
+console.log(findMedianSortedArrays(args.ones, args.twos));
--- iojs/4.medianOfTwoSortedArrays/test.js
+++ iojs/4.medianOfTwoSortedArrays/test.js
@@ -5,9 +5,40 @@ var should = chai.should();
 
 describe('median of two sorted arrays<https://leetcode.com/problems/median-of-two-sorted-arrays/>', function(){
 	var tests = [
-		{ones: [1,2,3,4,5,6,7], twos: [1,2,3,4,5,6], expected: 4},
+		{ones: [1,2,3,4,5,6,7], twos: [1,2,3,4,5,6,7], expected: 4},
+		{ones: [1,2,3,4,5,6], twos: [1,2,5,6], expected: 3.5},
+		{ones: [1,2,3,4,5,6], twos: [1,1,2,2,5,5,6,6], expected: 3.5},
+		{ones: [1,2,3,4,5,6,7], twos: [1,2,3,4,5,6,7,8], expected: 4},
+		{ones: [1,2,3,4,6,7,8,13], twos: [1,2,3,4,5,7,8,9,10,11,12], expected: 6},
 		{ones: [], twos: [1], expected: 1},
-		{ones: [1,2,3,4,5,6], twos: [], expected:3.5}
+		{ones: [1], twos: [1], expected: 1},
+		{ones: [100000], twos: [100001], expected: 100000.5},
+		{ones: [1,2], twos: [3,4], expected: 2.5},
+		{ones: [1,3], twos: [2,4], expected: 2.5},
+		{ones: [1,4], twos: [2,3], expected: 2.5},
+		{ones: [3,4], twos: [1,2], expected: 2.5},
+		{ones: [2,4], twos: [1,3], expected: 2.5},
+		{ones: [2,3], twos: [1,4], expected: 2.5},
+		{ones: [1,2,3,4,5,6], twos: [], expected:3.5},
+		{ones: [1,2,3,4,5,6,7], twos: [], expected:4},
+		{ones: [1,2,3,4,5], twos: [6,7,8,9,10,11,12,13,14,15], expected: 8},
+		{ones: [1,2,3,4,8], twos: [5,6,7,9,10,11,12,13,14,15], expected: 8},
+		{ones: [1,2,3,4,5], twos: [6,7,8,9,10,11,12,13,14,15,16], expected: 8.5},
+		{ones: [1,2,3,8,9], twos: [4,5,6,7,10,11,12,13,14,15,16], expected: 8.5},
+		{ones: [1,2,3,4,8], twos: [5,6,7,9,10,11,12,13,14,15,16], expected: 8.5},
+		{ones: [1,2,3,4,9], twos: [5,6,7,8,10,11,12,13,14,15,16], expected: 8.5},
+		{ones: [11,12,13,14,15], twos: [1,2,3,4,5,6,7,8,9,10], expected: 8},
+		{ones: [8,12,13,14,15], twos: [1,2,3,4,5,6,7,9,10,11], expected: 8},
+		{ones: [11,12,13,14,15,16], twos: [1,2,3,4,5,6,7,8,9,10], expected: 8.5},
+		{ones: [8,9,13,14,15,16], twos: [1,2,3,4,5,6,7,11,12,10], expected: 8.5},
+		{ones: [9,12,13,14,15,16], twos: [1,2,3,4,5,6,7,8,10,11], expected: 8.5},
+		{ones: [8,12,13,14,15,16], twos: [1,2,3,4,5,6,7,9,10,11], expected: 8.5},
+		{ones: [1,1], twos: [1,2], expected: 1},
+		{ones: [1,2], twos: [1,2], expected: 1.5},
+		{ones: [2,3], twos: [1,4,5], expected: 3},
+		{ones: [2,3,6], twos: [1,4,5], expected: 3.5},
+		{ones: [2,3,4], twos: [1,5,6], expected: 3.5},
+		{ones: [2], twos: [1,2,3], expected: 2}
 	];
 	
 	describe('first solution', function(){
