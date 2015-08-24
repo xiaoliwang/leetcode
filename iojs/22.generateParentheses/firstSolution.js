619eafb105d53418897d93281168ec8effa49b8d
Tom <jiepengthegreat@126.com>
2015-08-24 23:35:54 +0800

    22题第一解

--- iojs/22.generateParentheses/firstSolution.js
+++ iojs/22.generateParentheses/firstSolution.js
@@ -3,7 +3,18 @@
  * @return {string[]}
  */
 var generateParenthesis = function(n) {
-    var stack = [];
-	var p = 1;
-	var q = 1;
+	function addingpar(parenthesis, str, n, m){
+		if (n == 0 && m ==0) {
+			parenthesis.push(str);
+			return;
+		}
+		if (m > 0) {addingpar(parenthesis, str + ')', n, m - 1);}
+		if (n > 0) {addingpar(parenthesis, str + '(', n - 1, m + 1);}
+	}
+	
+	var parenthesis = [];
+	addingpar(parenthesis, '', n , 0);
+	return parenthesis;
 };
+
+module.exports = generateParenthesis;
---
+++ iojs/22.generateParentheses/test.js
@@ -0,0 +1,22 @@
+var chai = require('chai'),
+	h1 = require('./firstSolution');
+var should = chai.should()
+	expect = chai.expect;
+
+describe('generate parentheses<https://leetcode.com/problems/generate-parentheses/>', function(){
+	var tests = [ 
+		{arg: 0, expected: ['']},
+		{arg: 1, expected: ['()']},
+		{arg: 2, expected: ['()()', '(())']},
+		{arg: 3, expected: ['()()()', '(()())', '((()))', '()(())', '(())()']}
+	];
+	
+	describe('first solution', function(){
+		tests.forEach(function(test, index){
+			it('args'+index, function(){
+				var code = h1(test.arg);
+				expect(code).to.have.members(test.expected);
+			});
+		});
+	});
+});
